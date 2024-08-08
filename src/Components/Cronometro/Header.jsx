634e9e56cc96b613b3e90163d12d2ca8f9cb384c
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-08-08 14:20:55 -0300

    incluindo um pouco de js

--- src/Components/Cronometro/Header.jsx
+++ src/Components/Cronometro/Header.jsx
@@ -1,15 +1,38 @@
-import React from "react"
 import * as S from "./HeaderStyle.jsx"
-// import React, { useState } from 'react'
+import React, { useState, useEffect } from "react"
 
 function Header() {
+
+    let [minutes, setMinutes] = useState(0)
+    let [hours, setHours] = useState(0)
+    let [seconds, setSeconds] = useState(50)
+    let intervalSeconds;
+
+    function start() {
+
+        intervalSeconds = setInterval(() => {
+            setSeconds(seconds += 1)
+            startMinutes()
+        }, 1000)   
+    }
+    
+    
+    function startMinutes() {
+        
+        if(seconds == 60) {
+            clearInterval(intervalSeconds)
+            setMinutes(minutes += 1)
+            setSeconds(seconds = 0)
+        }
+    }
+
     return(
         <S.StyleHeader>
             <S.Div>
-                <S.H2>00: 00: 00</S.H2>
+                <S.H2>{ hours }: { minutes }: { seconds }</S.H2>
             </S.Div>
             <S.Section>
-                <S.Button>
+                <S.Button onClick={start}>
                     INICIAR
                 </S.Button>
                 <S.Button>
